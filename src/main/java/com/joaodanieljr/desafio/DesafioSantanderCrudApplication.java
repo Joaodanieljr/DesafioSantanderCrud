43a091a76b68ec509a5b431fbfe141d489e4d551
Joao Junior <Joaodanieljr@gmail.com>
2021-03-12 19:52:15 -0300

    fisrt commit

---
+++ src/main/java/com/joaodanieljr/desafio/DesafioSantanderCrudApplication.java
@@ -0,0 +1,13 @@
+package com.joaodanieljr.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSantanderCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSantanderCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaodanieljr/desafio/DesafioSantanderCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodanieljr.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSantanderCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
