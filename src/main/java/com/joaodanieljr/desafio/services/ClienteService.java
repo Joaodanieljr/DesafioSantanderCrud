8cc385bc833b80082eaf105d106ae64684f4f8fa
joaodanieljr <joaodanieljr@gmail.com>
2021-03-14 11:43:40 -0300

    selecao de Pagina Cliente

--- src/main/java/com/joaodanieljr/desafio/services/ClienteService.java
+++ src/main/java/com/joaodanieljr/desafio/services/ClienteService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.joaodanieljr.desafio.Repository.ClienteRepository;
@@ -46,6 +49,10 @@ public class ClienteService {
 		return clienteRepository.findAll();
 	}
 	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pagerequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pagerequest);
+	}
 	
 	private void updateData (Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
